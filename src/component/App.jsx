83410061bbaa6e829a202864bece0f71345ee10b
nicoWeb31 <nico.riot@free.fr>
2020-10-09 10:04:04 +0200

    app

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -5,13 +5,32 @@ import React, { Component } from 'react'
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {
+            language : 'english'
+        };
     }
+
+
+    onlanguagechange = lang =>{
+        this.setState({language : lang})
+    }
+
+
+
     render() {
         return (
-            <>
-                App
-            </>
+            <div className ='ui container'>
+                <div className="">
+                    select a langage :
+                    <i className="flag us" onClick={()=>this.onlanguagechange('english')}></i>
+                    <i className="flag nl" onClick={()=>this.onlanguagechange('dutch')}></i>
+                </div>
+
+
+                
+
+
+            </div>
         );
     }
 }
