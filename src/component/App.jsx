c049596a7c2c2a24f09d7d451914241b747035a6
nicoWeb31 <nico.riot@free.fr>
2020-10-09 11:14:58 +0200

    context diff provider,default value

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import UserCreate from './UserCreate'
+import UserCreate from './UserCreate';
+
+//context
+import LanguageContext from '../context/LanguageContext';
 
 
 
@@ -7,30 +10,43 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            language : 'english'
+            language: 'english'
         };
     }
 
 
-    onlanguagechange = lang =>{
-        this.setState({language : lang})
+    onlanguagechange = lang => {
+        this.setState({ language: lang })
     }
 
 
 
     render() {
         return (
-            <div className ='ui container'>
+            <div className='ui container'>
                 <div className="">
                     select a langage :
-                    <i className="flag us" onClick={()=>this.onlanguagechange('english')}></i>
-                    <i className="flag nl" onClick={()=>this.onlanguagechange('dutch')}></i>
+                    <i className="flag us" onClick={() => this.onlanguagechange('english')}></i>
+                    <i className="flag nl" onClick={() => this.onlanguagechange('dutch')}></i>
                 </div>
 
 
 
+                {/* provider tout ce qui est vrapp recoi via le provider les datas donner en value qu'on veut dans le context object*/}
 
-                <UserCreate/>
+                <h4>avec provider valeur du state</h4>
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
+
+
+                <h4>avec provider valeur en dur</h4>
+                <LanguageContext.Provider value='dutch'>
+                    <UserCreate />
+                </LanguageContext.Provider>
+
+                <h4>Sans provider valeur de l'object context par default</h4>
+                <UserCreate />
 
 
             </div>
--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,4 +1,4 @@
 import React from 'react'
 
-
+//default value util pour call api il me semble, context object en argument de createContext
 export default React.createContext('english');
